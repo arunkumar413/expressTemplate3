3691aff1e5c173fa9093b2544a29df64eb558051
arunkumar413 <arunkumar413@gmail.com>
2022-11-20 16:01:41 +0530

    session logic working as expected

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -1,22 +1,35 @@
 const { pool } = require("../dbConnection");
 
+var bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
+
 module.exports.Register = async function (req, res) {
   const client = await pool.connect();
 
+  let salt = await bcrypt.genSalt(12);
+  let hash = await bcrypt.hash("password", salt);
+  // https://www.npmjs.com/package/bcryptjs
+
   try {
     let query = {
-      text: "INSERT INTO public.user (name, email,isEmailVerified) VALUES($1, $2,$3) RETURNING *",
-      values: ["Arun", "arunkumar413@gmail.hin", 1],
+      text: "INSERT INTO public.user (first_name,last_name, email,password,isemailverified) VALUES($1, $2,$3,$4,$5) RETURNING *",
+      values: ["Arun Kumar", "Kadari", "arunkumar413@gmail.k", hash, false],
     };
 
     let dbres = await client.query(query);
+    currentUserId = dbres.rows[0].id;
 
     //generate session id and set it to cookie value
     // send verification email
-    //
+    let sessionID = uuidv4();
 
-    console.log(dbres.rows);
-    res.cookie("rememberme", "1", {
+    let sessionQuery = {
+      text: "INSERT INTO public.session (email,userid,sessionid) VALUES ($1,$2,$3) returning *",
+      values: ["arunkumar413@gmail.k", currentUserId, sessionID],
+    };
+
+    let sessionResult = await client.query(sessionQuery);
+    res.cookie("sessionid", sessionResult.rows[0].sessionid, {
       expires: new Date(Date.now() + 900000),
       httpOnly: true,
     });
